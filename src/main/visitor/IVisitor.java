247b02561e2e62e691f13bdd63fa7303183e7f52
mahdiyari15 <mohammadmahdiyari@ut.ac.ir>
2026-06-03 23:06:25 +0330

    Complete IVisitor and Visitor interfaces with all missing AST nodes

--- src/main/visitor/IVisitor.java
+++ src/main/visitor/IVisitor.java
@@ -6,18 +6,52 @@ import main.ast.types.*;
 import main.ast.declarations.*;
 import main.ast.statements.*;
 import main.ast.expressions.*;
-import main.ast.expressions.literals.*;
 
 public interface IVisitor<T> {
     T visit(Program program);
-
     T visit(Module module);
-
     T visit(ModuleDecl moduleDecl);
-
     T visit(Struct struct);
-
     T visit(StructDecl structDecl);
+    
+    // change
+    // Declarations & Core
+    T visit(Method method);
+    T visit(Parameter parameter);
+    T visit(Var varDeclaration);
+    T visit(Identifier identifier);
 
+    // change
+    // Statements
+    T visit(AssignmentStmt assignmentStmt);
+    T visit(Block block);
+    T visit(BreakStmt breakStmt);
+    T visit(ContinueStmt continueStmt);
+    T visit(ForStmt forStmt);
+    T visit(IfStmt ifStmt);
+    T visit(InputStmt inputStmt);
+    T visit(MethodCallStmt methodCallStmt);
+    T visit(OutputStmt outputStmt);
+    T visit(ReturnStmt returnStmt);
+    T visit(VarDeclStmt varDeclStmt);
+    T visit(WhileStmt whileStmt);
 
+    // change
+    // Expressions
+    T visit(BinaryExpression binaryExpression);
+    T visit(UnaryExpression unaryExpression);
+    T visit(MethodCall methodCall);
+    T visit(ConstructorCall constructorCall);
+    T visit(SimpleIdentifier simpleIdentifier);
+    T visit(FieldAccess fieldAccess);
+    T visit(ThisAccess thisAccess);
+
+    // change
+    // Literals
+    T visit(IntValue intValue);
+    T visit(FloatValue floatValue);
+    T visit(BoolValue boolValue);
+    T visit(CharValue charValue);
+    T visit(DoubleValue doubleValue);
+    T visit(StringValue stringValue);
 }
--- src/main/visitor/Visitor.java
+++ src/main/visitor/Visitor.java
@@ -6,23 +6,75 @@ import main.ast.types.*;
 import main.ast.declarations.*;
 import main.ast.statements.*;
 import main.ast.expressions.*;
-import main.ast.expressions.literals.*;
 
 public abstract class Visitor<T> implements IVisitor<T> {
-    @Override
+    @Override 
     public T visit(Program program) { return null; }
-
-    @Override
+    @Override 
     public T visit(Module module) { return null; }
-
-    @Override
+    @Override 
     public T visit(ModuleDecl moduleDecl) { return null; }
-
-    @Override
+    @Override 
     public T visit(Struct struct) { return null; }
-
-    @Override
+    @Override 
     public T visit(StructDecl structDecl) { return null; }
-
-
+    // change
+    @Override 
+    public T visit(Method method) { return null; }
+    @Override 
+    public T visit(Parameter parameter) { return null; }
+    @Override 
+    public T visit(Var varDeclaration) { return null; }
+    @Override 
+    public T visit(Identifier identifier) { return null; }
+    @Override 
+    public T visit(AssignmentStmt assignmentStmt) { return null; }
+    @Override 
+    public T visit(Block block) { return null; }
+    @Override 
+    public T visit(BreakStmt breakStmt) { return null; }
+    @Override 
+    public T visit(ContinueStmt continueStmt) { return null; }
+    @Override 
+    public T visit(ForStmt forStmt) { return null; }
+    @Override 
+    public T visit(IfStmt ifStmt) { return null; }
+    @Override 
+    public T visit(InputStmt inputStmt) { return null; }
+    @Override 
+    public T visit(MethodCallStmt methodCallStmt) { return null; }
+    @Override 
+    public T visit(OutputStmt outputStmt) { return null; }
+    @Override 
+    public T visit(ReturnStmt returnStmt) { return null; }
+    @Override 
+    public T visit(VarDeclStmt varDeclStmt) { return null; }
+    @Override 
+    public T visit(WhileStmt whileStmt) { return null; }
+    @Override 
+    public T visit(BinaryExpression binaryExpression) { return null; }
+    @Override 
+    public T visit(UnaryExpression unaryExpression) { return null; }
+    @Override 
+    public T visit(MethodCall methodCall) { return null; }
+    @Override 
+    public T visit(ConstructorCall constructorCall) { return null; }
+    @Override 
+    public T visit(SimpleIdentifier simpleIdentifier) { return null; }
+    @Override 
+    public T visit(FieldAccess fieldAccess) { return null; }
+    @Override 
+    public T visit(ThisAccess thisAccess) { return null; }
+    @Override 
+    public T visit(IntValue intValue) { return null; }
+    @Override 
+    public T visit(FloatValue floatValue) { return null; }
+    @Override 
+    public T visit(BoolValue boolValue) { return null; }
+    @Override 
+    public T visit(CharValue charValue) { return null; }
+    @Override 
+    public T visit(DoubleValue doubleValue) { return null; }
+    @Override 
+    public T visit(StringValue stringValue) { return null; }
 }

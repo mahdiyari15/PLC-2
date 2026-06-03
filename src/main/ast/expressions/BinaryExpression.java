9fb82393e14b4e2d6563989c3abd5fb698125489
mahdiyari15 <mohammadmahdiyari@ut.ac.ir>
2026-06-03 23:09:58 +0330

    Z

---
+++ src/main/ast/expressions/BinaryExpression.java
@@ -0,0 +1,29 @@
+package main.ast.expressions; // change
+
+import main.visitor.IVisitor;
+
+public class BinaryExpression extends Expression {
+    private Expression left;
+    private Expression right;
+    private BinaryOperator binaryOperator;
+
+    public BinaryExpression(Expression left, Expression right, BinaryOperator binaryOperator) {
+        this.left = left;
+        this.right = right;
+        this.binaryOperator = binaryOperator;
+    }
+
+    public Expression getLeft() { return left; }
+    public void setLeft(Expression left) { this.left = left; }
+
+    public Expression getRight() { return right; }
+    public void setRight(Expression right) { this.right = right; }
+
+    public BinaryOperator getBinaryOperator() { return binaryOperator; }
+    public void setBinaryOperator(BinaryOperator binaryOperator) { this.binaryOperator = binaryOperator; }
+
+    @Override
+    public <T> T accept(IVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
---
+++ src/main/ast/expressions/BinaryOperator.java
@@ -0,0 +1,5 @@
+package main.ast.expressions; // change
+
+public enum BinaryOperator {
+    ADD, SUB, MULT, DIV, GT, LT, GTE, LTE, EQ, NEQ, AND, OR
+}
